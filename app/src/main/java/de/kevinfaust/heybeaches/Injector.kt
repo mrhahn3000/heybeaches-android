3c35b80d867505b80246c3b77115119ab8805078
Kevin Faust <kevin.faust@daimler.com>
2018-06-28 12:27:57 +0200

    put it together in injector

--- app/src/main/java/de/kevinfaust/heybeaches/Injector.kt
+++ app/src/main/java/de/kevinfaust/heybeaches/Injector.kt
@@ -1,19 +1,34 @@
 package de.kevinfaust.heybeaches
 
-import de.kevinfaust.heybeaches.api.IApiClient
-import de.kevinfaust.heybeaches.api.user.UserApiClient
+import android.content.Context
 import de.kevinfaust.heybeaches.api.services.IWebService
 import de.kevinfaust.heybeaches.api.services.WebService
+import de.kevinfaust.heybeaches.api.user.UserApiClient
+import de.kevinfaust.heybeaches.repository.IUserRepository
+import de.kevinfaust.heybeaches.repository.UserRepository
+import de.kevinfaust.heybeaches.services.PersistenceService
 import java.util.concurrent.Executors
 
 object Injector {
 
-    private fun userApiClient(): IApiClient {
+    private fun userRepository(context: Context): IUserRepository {
+        val userRepository = UserRepository
+        userRepository.apiClient = Injector.userApiClient()
+        userRepository.persistenceService = Injector.persistenceService(context)
+
+        return userRepository
+    }
+
+    private fun userApiClient(): UserApiClient {
         val userApiClient = UserApiClient
         userApiClient.webService = Injector.webService()
         userApiClient.executorService = Executors.newCachedThreadPool()
 
-        return UserApiClient
+        return userApiClient
+    }
+
+    private fun persistenceService(context: Context): PersistenceService {
+        return PersistenceService(context)
     }
 
     private fun webService(): IWebService {
